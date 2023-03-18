40595fa5881a81e813e0c702a00379b904c04000
tokai235 <the.summer.of.spiritual@gmail.com>
2023-03-18 23:24:05 +0900

    コメント追加

--- chatgpt_utils.py
+++ chatgpt_utils.py
@@ -23,6 +23,7 @@ def generate_reply_text(reply_text):
     # logger.logger.info(text)
     # return text
 
+# chatGPTの人格注入のためのプロンプトをテキストファイルから読み込む
 def read_prompt(filename):
     f = open(filename, 'r')
     text = f.read()
