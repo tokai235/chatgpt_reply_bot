6d5ef7dde0899780f010faf087042ba555034d0c
tokai235 <the.summer.of.spiritual@gmail.com>
2023-03-18 22:55:49 +0900

    とりあえずchatgptでテキスト生成するまで

--- chatgpt_utils.py
+++ chatgpt_utils.py
@@ -1,8 +1,25 @@
+import os
 import logger
+import openai
+
+openai.api_key = os.environ["OPENAI_API_KEY"]
 
 def generate_reply_text(reply_text):
+    reply_text = "こんにちは"
     logger.logger.info("\n=== generate_reply_text reply_text ===")
     logger.logger.info(reply_text)
-    text = "リプライありがとう！"
-    logger.logger.info(text)
-    return text
+
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "関西弁で話して"},
+            {"role": "system", "content": "発言の最後に「知らんけど。」をつけて"},
+            {"role": "user", "content": reply_text},
+        ],
+    )
+    logger.logger.info("\n=== generate_reply_text response ===")
+    print(response.choices[0]["message"]["content"].strip())
+
+    # text = "リプライありがとう！"
+    # logger.logger.info(text)
+    # return text
