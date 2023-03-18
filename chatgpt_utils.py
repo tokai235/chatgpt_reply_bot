6ec3fc5702ac3ed796ebff589132c87ca1e57aed
tokai235 <the.summer.of.spiritual@gmail.com>
2023-03-19 01:57:00 +0900

    メソッドを整理

--- chatgpt_utils.py
+++ chatgpt_utils.py
@@ -4,21 +4,20 @@ import config
 
 openai.api_key = config.openai_api_key
 
-def generate_reply_text(reply_text):
-    reply_text = "こんにちは"
-    logger.logger.info("=== generate_reply_text reply_text ===")
-    logger.logger.info(reply_text)
+def generate_reply_text(text):
+    text = "テストです"
+    logger.logger.info("=== generate_reply_text text ===")
+    logger.logger.info(text)
 
     response = openai.ChatCompletion.create(
         model="gpt-3.5-turbo",
         messages=[
             {"role": "system", "content": config.prompt},
-            {"role": "user", "content": reply_text},
+            {"role": "user", "content": text},
         ],
     )
     logger.logger.info("=== generate_reply_text response ===")
-    print(response.choices[0]["message"]["content"].strip())
+    logger.logger.info(response)
 
-    # text = "リプライありがとう！"
-    # logger.logger.info(text)
-    # return text
+    reply = response.choices[0]["message"]["content"]
+    return reply
